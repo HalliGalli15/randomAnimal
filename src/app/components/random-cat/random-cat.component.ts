af338dfec8da0fb7ce1f1d49ecb3592ee24e1915
Damian Hug <damian.hug@outlook.com>
2021-03-09 11:15:36 +0100

    services and mainCat-Component

---
+++ src/app/components/random-cat/random-cat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-random-cat',
+  templateUrl: './random-cat.component.html',
+  styleUrls: ['./random-cat.component.scss']
+})
+export class RandomCatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
